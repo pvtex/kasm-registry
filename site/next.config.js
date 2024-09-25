6c0dc04299c87d3298a3dd6496aedfd0e2fe3cb9
PVTex <info@pvtex.de>
2024-09-25 17:37:49 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    name: 'PVTex',
+    description: 'PVTex KASMweb Repo.',
+    icon: '/img/logo.png',
+    listUrl: 'https://pvtex.github.io/',
+    contactUrl: 'https://www.pvtex.de',
   },
   reactStrictMode: true,
   swcMinify: true,
